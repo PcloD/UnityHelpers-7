425274579ed179e430944c5bec0f5acb0d5424ac
PentagramPro <maxon-line@yandex.ru>
2015-02-10 01:05:31 +0300

    Added some new common controllers

---
+++ Controllers/NumericFieldController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumericFieldController : MonoBehaviour {
+
+	public Color NegativeColor = new Color(1,0.3f,0.3f);
+	public Color PositiveColor = new Color(0.3f,1,0.3f);
+
+	float Speed = 1000;
+	public float TimeToEquality = 1;
+	Text text;
+	int fieldValue = 0;
+	float displayedValue = 0;
+
+	public bool runMode = true;
+	public bool useColors = false;
+	protected void Awake ()
+	{
+	
+		text = GetComponent<Text>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		UpdateText();
+	}
+
+	void OnEnable()
+	{
+		displayedValue = 0;
+		UpdateSpeed();
+	}
+	// Update is called once per frame
+	void Update () {
+		if(displayedValue!=fieldValue)
+		{
+			if(runMode)
+			{
+				float  iDelta = fieldValue - displayedValue;
+				float increment = Speed*Time.smoothDeltaTime;
+
+				iDelta=Mathf.Sign(iDelta)*Mathf.Min( increment,Mathf.Abs(iDelta));
+
+				displayedValue+=iDelta;
+			}
+			else
+			{
+				displayedValue = fieldValue;
+			}
+			UpdateText();
+		}
+	}
+
+	public int Value{
+		get{
+			return fieldValue;
+		}
+		set{
+			fieldValue = value;
+			UpdateSpeed();
+		}
+	}
+	public void AddValue(int delta)
+	{
+		fieldValue+=delta;
+		UpdateSpeed();
+	}
+
+	protected void UpdateSpeed()
+	{
+		float delta = Mathf.Abs(fieldValue-displayedValue);
+		Speed = delta/TimeToEquality;
+	}
+
+	protected void UpdateText()
+	{
+		text.text = ((int)displayedValue).ToString("#,##0");
+		if(useColors)
+		{
+			if(displayedValue>=0)
+				text.color = PositiveColor;
+			else
+				text.color = NegativeColor;
+		}
+	}
+}
---
+++ Extensions/EnumExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class EnumExtension {
+
+	// Extends enums with Next method. This method returns next enum value in declaration order
+	public static T Next<T>(this T src) where T : struct
+	{
+		if (!typeof(T).IsEnum) throw new ArgumentException(String.Format("Argumnent {0} is not an Enum", typeof(T).FullName));
+		
+		T[] Arr = (T[])Enum.GetValues(src.GetType());
+		int j = Array.IndexOf<T>(Arr, src) + 1;
+		return (Arr.Length==j) ? Arr[0] : Arr[j];            
+	}
+}
