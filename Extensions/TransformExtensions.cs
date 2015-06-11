334927cc81a098862035580ae2efbd94c43ba200
PentagramPro <maxon-line@yandex.ru>
2015-06-12 02:50:45 +0300

    New extension for transform class

---
+++ Extensions/TransformExtensions.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+public static class TransformEx {
+	public static Transform Clear(this Transform transform)
+	{
+		foreach (Transform child in transform) {
+			GameObject.Destroy(child.gameObject);
+		}
+		return transform;
+	}
+}
